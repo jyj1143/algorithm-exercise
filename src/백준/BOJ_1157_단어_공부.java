d9844e65ca3aff46bfde59899bd04b134ce2e559
jyj1143 <jyj1143@gmail.com>
2025-12-07 23:56:49 +0900

    FEAT: [BOJ_1157] Title: 단어공부

---
+++ src/백준/BOJ_1157_단어_공부.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ_1157_단어_공부 {
+    public static void main(String[] args ) throws IOException{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        Integer[] alpha = new Integer[26];
+        Arrays.fill(alpha, 0);
+
+        for(int i=0;i<str.length();i++){
+            int index = Character.toUpperCase(str.charAt(i)) - 'A';
+            alpha[index]++;
+        }
+
+        counter(alpha);
+
+        br.close();
+    }
+
+    public static void counter(Integer[] alpha){
+       int max = 0;
+       char answer = '?';
+       boolean isDuplicated = false;
+
+
+       for(int i=0;i<alpha.length;i++){
+        if(alpha[i] == max){
+            isDuplicated = true;
+        } else if(alpha[i] > max){
+            max = alpha[i];
+            answer = (char)(i + 'A');
+            isDuplicated = false;
+        }
+       }
+
+       if(isDuplicated){
+        System.out.println("?");
+       }else {
+        System.out.println(answer);
+       }
+
+       
+    }
+}
